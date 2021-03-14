6a772ccb75c8e91bdc19d78e559228e4aa262f80
Yash Tank <yashtank09@gmail.com>
2021-03-14 16:25:03 +0530

    Comparable Interface Done

---
+++ comparable_interface2.java
@@ -0,0 +1,14 @@
+import java.util.*;  
+class comparable_interface2{  
+    public static void main(String args[]){  
+    ArrayList<Student> al=new ArrayList<Student>();  
+    al.add(new Student(101,"Vijay",23));  
+    al.add(new Student(106,"Ajay",27));  
+    al.add(new Student(105,"Jai",21));  
+    
+    Collections.sort(al);  
+    for(Student st:al){  
+    System.out.println(st.rollno+" "+st.name+" "+st.age);  
+}  
+}  
+}  
